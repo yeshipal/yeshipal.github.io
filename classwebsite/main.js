ed011131f8aae87b4a34b7b5a0376dc58f0c8a25
Yeshi Paljor <92138464+yeshipal@users.noreply.github.com>
2024-07-30 09:47:28 -0400

    home link

--- classwebsite/main.js
+++ classwebsite/main.js
@@ -39,3 +39,23 @@ function typeTitle() {
         }
 
 typeTitle();
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    let button = document.getElementById("scrollToTopButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
